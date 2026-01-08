41b36aa7857da31535acf0162b0c2e1701a50602
Pakata Goh <pakatagohlh@gmail.com>
2026-01-09 01:40:09 +0800

    use privacy disclaimer component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import { ClientOnly, createFileRoute } from "@tanstack/react-router";
 import { AddVoucherForm } from "@/components/voucher/AddVoucherForm";
 import { SavedVouchersList } from "@/components/voucher/SavedVouchersList";
+import PrivacyDisclaimer from "@/components/PrivacyDisclaimer";
 
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
 	return (
 		<div className="mx-auto max-w-7xl px-4 py-8 md:px-6">
+			<PrivacyDisclaimer />
 			<AddVoucherForm />
 			<ClientOnly>
 				<SavedVouchersList />
