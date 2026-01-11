ecddf196bb7bbe7a27f1d2916973ad5375aaa7d5
Pakata Goh <pakatagohlh@gmail.com>
2026-01-11 13:20:26 +0800

    format vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,48 +1,49 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
+import netlify from "@netlify/vite-plugin-tanstack-start";
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-// import { nitro } from 'nitro/vite'
-import netlify from '@netlify/vite-plugin-tanstack-start'
+import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import viteTsConfigPaths from "vite-tsconfig-paths";
 
 const config = defineConfig({
-  build: {
-    sourcemap: true, // Source map generation must be turned on for sentry vite plugin
-  },
-  define: {
-    // Inject SENTRY_RELEASE as a compile-time constant for client-side code
-    'import.meta.env.VITE_SENTRY_RELEASE': JSON.stringify(process.env.SENTRY_RELEASE || 'development'),
-  },
-  plugins: [
-    devtools(),
-    // nitro(),
-    netlify(), 
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    tailwindcss(),
-    tanstackStart(),
-    viteReact(),
-    sentryVitePlugin({
-      authToken: process.env.SENTRY_AUTH_TOKEN,
-      org: "personal-kqc",
-      project: "sg-vouchers",
-      release: {
-        name: process.env.SENTRY_RELEASE, // Will be set during build
-      },
-      sourcemaps: {
-        filesToDeleteAfterUpload: [
-          "./**/*.map",
-          ".*/**/public/**/*.map",
-          "./dist/**/client/**/*.map",
-        ],
-      },
-    }),
-  ],
-})
+	build: {
+		sourcemap: true, // Source map generation must be turned on for sentry vite plugin
+	},
+	define: {
+		// Inject SENTRY_RELEASE as a compile-time constant for client-side code
+		"import.meta.env.VITE_SENTRY_RELEASE": JSON.stringify(
+			process.env.SENTRY_RELEASE || "development",
+		),
+	},
+	plugins: [
+		devtools(),
+		// nitro(),
+		netlify(),
+		// this is the plugin that enables path aliases
+		viteTsConfigPaths({
+			projects: ["./tsconfig.json"],
+		}),
+		tailwindcss(),
+		tanstackStart(),
+		viteReact(),
+		sentryVitePlugin({
+			authToken: process.env.SENTRY_AUTH_TOKEN,
+			org: "personal-kqc",
+			project: "sg-vouchers",
+			release: {
+				name: process.env.SENTRY_RELEASE, // Will be set during build
+			},
+			sourcemaps: {
+				filesToDeleteAfterUpload: [
+					"./**/*.map",
+					".*/**/public/**/*.map",
+					"./dist/**/client/**/*.map",
+				],
+			},
+		}),
+	],
+});
 
-export default config
+export default config;
